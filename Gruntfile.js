761c5aa8476fd3e9b3bf3a3bf7352a10ec0907de
Diego Lirio <ddamaceno@dellavolpe.com.br>
2017-05-18 15:53:03 -0300

    Exclude node_modules

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+module.exports = function(grunt) {
+  grunt.initConfig({
+		concat : {
+		  options : {
+			sourceMap :true
+		  },
+		  dist : {
+			src  : ['node_modules/jquery/dist/jquery.min.js', 
+					'node_modules/materialize-css/dist/js/materialize.min.js', 
+					'node_modules/angular/angular.min.js',
+					'node_modules/angular-route/angular-route.min.js',
+					'src/fenix.js',
+					'src/controllers/oficina-cadastro.controller.js',
+					'src/controllers/endereco-cadastro.controller.js'],
+			dest : 'src/dist/core.js'
+		  }
+		},
+		uglify : {
+		  //options : {
+			//sourceMap : true,
+			//sourceMapIncludeSources : true
+		  //}
+		  //,
+		  dist : {
+			src  : '<%= concat.dist.dest %>',
+			dest : 'src/dist/core.min.js'
+		  }
+		},
+		watch: {
+			scripts: {
+				files: ['**/*.js'],
+				tasks: ['build'],
+				options: {
+					spawn: false,
+				},
+			},
+		}
+  }); 
+ 
+  // carrega plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  grunt.registerTask('build', ['concat', 'uglify']);
+};
